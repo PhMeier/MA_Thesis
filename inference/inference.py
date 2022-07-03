cda9d3059b9a4747eea8d8eda8c8629422d0da2e
PhMeier <junk12345671@aol.com>
2022-07-03 11:18:28 +0200

    [TOW] adpated inference

--- inference/inference.py
+++ inference/inference.py
@@ -2,7 +2,7 @@ import evaluate
 import torch
 from datasets import load_dataset, load_metric
 from evaluate import evaluator
-from transformers import AutoTokenizer, pipeline
+from transformers import AutoTokenizer, pipeline, Trainer
 from torch.utils.data import DataLoader
 import datasets
 import numpy as np
@@ -35,19 +35,27 @@ def evaluate(model, test_data):
     acc = acc["accuracy"]
     print(acc)
 """
-
+def compute_metrics(p): #eval_pred):
+    metric_acc = datasets.load_metric("accuracy")
+    preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
+    preds = np.argmax(preds, axis=1)
+    result = {}
+    result["accuracy"] = metric_acc.compute(predictions=preds, references=p.label_ids)["accuracy"]
+    return result
 
 if __name__ == "__main__":
     path = "/workspace/students/meier/MA/SOTA_Bart/best/checkpoint-12000/" #"../checkpoint-12000/"
     #model = torch.load(path+"pytorch_model.bin", map_location=torch.device('cpu'))
     model = BartForSequenceClassification.from_pretrained(path, local_files_only=True)
     dataset_test_split = load_dataset("glue", "mnli", split='test_matched')
-    #tokenized_datasets_test = dataset_test_split.map(encode, batched=True) 
-    
-    dataset_test_split = dataset_test_split.rename_column("premise", "text")
-    dataset_test_split = dataset_test_split.rename_column("hypothesis", "text")
+    tokenized_datasets_test = dataset_test_split.map(encode, batched=True)
 
+    #dataset_test_split = dataset_test_split.rename_column("premise", "text")
+    #dataset_test_split = dataset_test_split.rename_column("hypothesis", "text")
 
+    trainer = Trainer(model=model, tokenizer=tokenizer, compute_metrics=compute_metrics)
+
+    """
     pipe = pipeline("sentiment-analysis", model=model, device=0, tokenizer=tokenizer)
     metric = evaluate.load("accuracy")
     eval1 = evaluator("sentiment-analysis")
@@ -56,7 +64,7 @@ if __name__ == "__main__":
                            label_mapping={"Contradiction": -1, "Neutral": 0, "Entailment":1}, input_column="text")
 
     print(results)
-    """
+    
     tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large")
     tokenized_datasets_t = dataset_test_split.map(encode, batched=True)
     tokenized_datasets_t = tokenized_datasets_t.rename_column("label", "labels")
