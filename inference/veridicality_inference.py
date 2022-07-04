5bd117de172d7c8ef7dc82c08d4bad05cf7a56de
PhMeier <junk12345671@aol.com>
2022-07-04 16:37:16 +0200

    [TOW] added verid inf

---
+++ inference/veridicality_inference.py
@@ -0,0 +1,37 @@
+import evaluate
+import torch
+from datasets import load_dataset, load_metric
+from evaluate import evaluator
+from transformers import AutoTokenizer, pipeline, Trainer
+from torch.utils.data import DataLoader
+import datasets
+import numpy as np
+from transformers import AutoTokenizer, BartForSequenceClassification
+
+tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large")
+
+def encode(examples):
+    return tokenizer(examples['premise'], examples['hypothesis'], truncation=True, padding='max_length')#, max_length="max_length")
+
+
+def compute_metrics(p): #eval_pred):
+    metric_acc = datasets.load_metric("accuracy")
+    preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
+    preds = np.argmax(preds, axis=1)
+    result = {}
+    result["accuracy"] = metric_acc.compute(predictions=preds, references=p.label_ids)["accuracy"]
+    return result
+
+
+
+if __name__ == "__main__":
+    path = "/workspace/students/meier/MA/SOTA_Bart/best/checkpoint-12000/" #"../checkpoint-12000/"
+    #model = torch.load(path+"pytorch_model.bin", map_location=torch.device('cpu'))
+    model = BartForSequenceClassification.from_pretrained(path, local_files_only=True)
+    dataset_test_split = load_dataset("csv", data_files="../preprocess/verb_verid_neg.csv")
+    tokenized_datasets_test = dataset_test_split.map(encode, batched=True)
+    tokenized_datasets_test = tokenized_datasets_test.rename_column("signature", "label")
+    tokenized_datasets_test = tokenized_datasets_test.rename_column("sentence", "premise")
+    tokenized_datasets_test = tokenized_datasets_test.rename_column("complement", "hypothesis")
+    trainer = Trainer(model=model, tokenizer=tokenizer, compute_metrics=compute_metrics)
+    trainer.train()
