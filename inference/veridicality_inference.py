7f9d7f01e7f5e6676a8a6ea27d382a2cfd092890
PhMeier <junk12345671@aol.com>
2022-07-18 09:33:41 +0000

    veridicality inf

--- inference/veridicality_inference.py
+++ inference/veridicality_inference.py
@@ -11,7 +11,7 @@ from transformers import pipeline, TrainingArguments
 #import numpy as np
 #np.set_printoptions(threshold=np.inf)
 import pandas as pd
-
+CUDA_LAUNCH_BLOCKING=1
 tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large")
 
 
@@ -28,9 +28,17 @@ def compute_metrics(p):  # eval_pred):
     result["accuracy"] = metric_acc.compute(predictions=preds, references=p.label_ids)["accuracy"]
     return result
 
+def preprocess_logits(logits, labels):
+    if isinstance(logits, tuple):
+        # Depending on the model and config, logits may contain extra tensors,
+        # like past_key_values, but logits always come first
+        logits = logits[0]
+    return logits.argmax(dim=-1)
+
+
 
 if __name__ == "__main__":
-    paths = {"cl_data": "/home/students/meier/MA/MA_Thesis/preprocess/full_verb_veridicality.csv",
+    paths = {"cl_data": "/home/students/meier/MA/MA_Thesis/preprocess/test.csv", #full_verb_veridicality.csv",
              "cl_model": "/workspace/students/meier/MA/SOTA_Bart/best/checkpoint-12000/",
              "tow_model": "../checkpoint-12000/",
              "tow_data": "C:/Users/Meier/Projekte/MA_Thesis/preprocess/verb_verid_nor.csv"}
@@ -39,20 +47,21 @@ if __name__ == "__main__":
     path = "../checkpoint-12000/"  # "../checkpoint-12000/"
     # model = torch.load(path+"pytorch_model.bin", map_location=torch.device('cpu'))
     model = BartForSequenceClassification.from_pretrained(paths["cl_model"], local_files_only=True)
-    dataset_test_split = load_dataset("csv", data_files={"test": paths["cl_data"]})
+    #dataset_test_split = load_dataset("csv", data_files={"test": paths["cl_data"]}) 
+    dataset_test_split = load_dataset("glue", "mnli", split='test_matched')
+    dataset_test_split = dataset_test_split.remove_columns("label")
     #tokenized_datasets_test = dataset_test_split.rename_column("signature", "label")
     #tokenized_datasets_test = tokenized_datasets_test.rename_column("sentence", "premise")
     #tokenized_datasets_test = tokenized_datasets_test.rename_column("complement", "hypothesis")
     tokenized_datasets_test = dataset_test_split.map(encode, batched=True)
-    dataset_test_split = dataset_test_split.select(10)
-    targs = TrainingArguments(eval_accumulation_steps=10, per_device_eval_batch_size=2, output_dir="./")
-    trainer = Trainer(model=model, tokenizer=tokenizer, compute_metrics=compute_metrics, args=targs)
+    targs = TrainingArguments(eval_accumulation_steps=10, per_device_eval_batch_size=8, output_dir="./")
+    trainer = Trainer(model=model, tokenizer=tokenizer, args=targs, preprocess_logits_for_metrics=preprocess_logits) #compute_metrics=compute_metrics
     # trainer.evaluate()
     model.eval()
-    res = trainer.predict(tokenized_datasets_test["test"])
+    res = trainer.predict(tokenized_datasets_test) #["test"])
     print(res)
-
+    print(res.label_ids)
     #print(res.label_ids.reshape(107, 14).tolist())
-    pd.DataFrame(res.label_ids).to_csv("results.csv")
+    pd.DataFrame(res.predictions).to_csv("results_mnli_matched_bartLarge.csv")
     print(res.metrics)
 
