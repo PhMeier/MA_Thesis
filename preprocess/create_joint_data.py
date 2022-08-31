f2dddb2980b757a338ced88380ecf5ce373b19fc
PhMeier <junk12345671@aol.com>
2022-09-01 00:17:33 +0200

    [TOW] added finetuning scripts for mnli and mnli filtered

--- preprocess/create_joint_data.py
+++ preprocess/create_joint_data.py
@@ -67,7 +67,7 @@ def combine_data(filename):
 def combine_lists_premise(text_data, graph_data):
     result = []
     for t, g in zip(text_data, graph_data):
-        t = "<t> " + " </t>"
+        t = "<t> " + t + " </t>"
         result.append(t + " " + g)
     return result
 
@@ -75,7 +75,7 @@ def combine_lists_premise(text_data, graph_data):
 def combine_lists_hypothesis(text_data, graph_data):
     result = []
     for t, g in zip(text_data, graph_data):
-        t = "<t> " + " </t>"
+        t = "<t> "+ t + " </t>"
         g = g.replace("<g>", "")
         g = g + "</g>"
         result.append(g + " " + t)
