451f70072eb9ad524638cd2b2ec330bf0c46b571
PhMeier <junk12345671@aol.com>
2022-10-14 10:32:40 +0200

    [HD] weekend push

--- evaluation/compare_results.py
+++ evaluation/compare_results.py
@@ -39,9 +39,9 @@ def return_indices(content, count, signature):
     for num, line in enumerate(content[1:]):
         data_sig = line[14]
         if data_sig == signature:
-            index.append(num)
-    assert len(index) == count
+            index.append(line[0])
     print(len(index))
+    assert len(index) == count
     return index
 
 
@@ -91,13 +91,13 @@ if __name__ == "__main__":
     neutral_minus = return_indices(content, 28, signature="o/-\n")
     minus_neutral = return_indices(content, 55, signature="-/o\n")
     plus_neutral = return_indices(content, 80, signature="+/o\n")
-    neutral_neutral = return_indices(content, 935, signature="o/o\n")
+    neutral_neutral = return_indices(content, 935, signature="o/o\n") #935
 
     indices_dict = {"plus_plus": plus_plus, "plus_minus": plus_minus, "minus_plus": minus_plus, "neutral_plus": neutral_plus,
                     "neutral_minus": neutral_minus, "minus_neutral": minus_neutral, "plus_neutral": plus_neutral,
                     "neutral_neutral": neutral_neutral}
 
-    indices_key = "plus_neutral"
+    indices_key = "neutral_neutral"
     positive = "../preprocess/verb_verid_nor.csv"
     negative = "../preprocess/verb_verid_neg.csv"
     key_pos_or_neg = "neg"
@@ -107,13 +107,40 @@ if __name__ == "__main__":
     gold = pd.read_csv(file)
 
     # load the results file
+
+    # Positive results
+    """
+    results_42 = pd.read_csv("../results/veridical/AMRBART_veridicality_pos_text_3036.csv")#"BART_17_verid_neg_3036.csv")
+    results_17 = pd.read_csv("../results/veridical/AMRBART_17_veridicality_pos_text_2277.csv")#"BART_17_verid_neg_3036.csv")
+    results_67 = pd.read_csv("../results/veridical/AMRBART_67_veridicality_pos_text_5313.csv")#"BART_67_verid_neg_4554.csv")#"Bart_veridicality_neg_results_15175.csv")
+
+    # Graph
+    results_42_graph_only = pd.read_csv("../results/veridical/AMRBART_veridicality_pos_graph_only_2277.csv")
+    results_17_graph_only = pd.read_csv("../results/veridical/AMRBART_17_veridicality_pos_graph_only_3036.csv")
+    results_67_graph_only = pd.read_csv("../results/veridical/AMRBART_67_veridicality_pos_graph_only_3795.csv")
+
+    # Joint
+    results_42_joint = pd.read_csv("../results/veridical/AMRBART_verid_joint_pos_7590.csv")
+    results_17_joint = pd.read_csv("../results/veridical/AMRBART_17_verid_joint_pos_5313.csv")
+    results_67_joint = pd.read_csv("../results/veridical/AMRBART_67_verid_joint_pos_5313.csv")
+    """
+
+
+    # Text # neg
+
     results_42 = pd.read_csv("../results/veridical/AMRBART_veridicality_neg_text_3036.csv")#"BART_17_verid_neg_3036.csv")
     results_17 = pd.read_csv("../results/veridical/AMRBART_17_veridicality_neg_text_2277.csv")#"BART_17_verid_neg_3036.csv")
     results_67 = pd.read_csv("../results/veridical/AMRBART_67_veridicality_neg_text_5313.csv")#"BART_67_verid_neg_4554.csv")#"Bart_veridicality_neg_results_15175.csv")
 
+    # Graph
     results_42_graph_only = pd.read_csv("../results/veridical/AMRBART_veridicality_neg_graph_only_2277.csv")
     results_17_graph_only = pd.read_csv("../results/veridical/AMRBART_17_veridicality_neg_graph_only_3036.csv")
     results_67_graph_only = pd.read_csv("../results/veridical/AMRBART_67_veridicality_neg_graph_only_3795.csv")
+
+    # Joint
+    results_42_joint = pd.read_csv("../results/veridical/AMRBART_verid_joint_neg_7590.csv")
+    results_17_joint = pd.read_csv("../results/veridical/AMRBART_17_verid_joint_neg_5313.csv")
+    results_67_joint = pd.read_csv("../results/veridical/AMRBART_67_verid_joint_neg_5313.csv")
 
     #print(results.head())
     #df.rename(index={0:"Index", 1:"label"})
@@ -121,29 +148,45 @@ if __name__ == "__main__":
     res42_failed, predictions_42 = indepth_results(gold, results_42, indices_dict[indices_key])
     res17_failed, predictions_17 = indepth_results(gold, results_17, indices_dict[indices_key])
     res67_failed, predictions_67 = indepth_results(gold, results_67, indices_dict[indices_key])
-
+    # graph
     res42_failed_graph_only, predictions_42_graph = indepth_results(gold, results_42_graph_only, indices_dict[indices_key])
     res17_failed_graph_only, predictions_17_graph = indepth_results(gold, results_17_graph_only, indices_dict[indices_key])
     res67_failed_graph_only, predictions_67_graph = indepth_results(gold, results_67_graph_only, indices_dict[indices_key])
+    # joint
+    results_42_joint_failed, predictions_42_joint = indepth_results(gold, results_42_joint, indices_dict[indices_key])
+    results_17_joint_failed, predictions_17_joint = indepth_results(gold, results_17_joint, indices_dict[indices_key])
+    results_67_joint_failed, predictions_67_joint = indepth_results(gold, results_67_joint, indices_dict[indices_key])
+
 
     res42_failed_tuples = [tuple(lst) for lst in res42_failed]
     res17_failed_tuples = [tuple(lst) for lst in res17_failed]
     res67_failed_tuples = [tuple(lst) for lst in res67_failed]
-
+    # graph
     res42_failed_tuples_graph_only = [tuple(lst) for lst in res42_failed_graph_only]
     res17_failed_tuples_graph_only = [tuple(lst) for lst in res17_failed_graph_only]
     res67_failed_tuples_graph_only = [tuple(lst) for lst in res67_failed_graph_only]
+    # joint
+    res42_failed_tuples_joint = [tuple(lst) for lst in results_42_joint_failed]
+    res17_failed_tuples_joint = [tuple(lst) for lst in results_17_joint_failed]
+    res67_failed_tuples_joint = [tuple(lst) for lst in results_67_joint_failed]
+
+
 
     res42_failed_set = set(res42_failed_tuples)
     res17_failed_set = set(res17_failed_tuples)
     res67_failed_set = set(res67_failed_tuples)
-
+    # graph
     res42_failed_set_graph_only = set(res42_failed_tuples_graph_only)
     res17_failed_set_graph_only = set(res17_failed_tuples_graph_only)
     res67_failed_set_graph_only = set(res67_failed_tuples_graph_only)
+    # joint
+    res42_failed_set_joint = set(res42_failed_tuples_joint)
+    res17_failed_set_joint = set(res17_failed_tuples_joint)
+    res67_failed_set_joint = set(res67_failed_tuples_joint)
 
 
-    print("Common Errors: \n", *res42_failed_set.intersection(res17_failed_set, res67_failed_set), sep="\n")
+
+
     print("\n Length of errors sets (text): {}, {}, {} Sum: {}".format(len(res42_failed_set), len(res17_failed_set),
                                                                        len(res67_failed_set), len(res42_failed_set) +len(res17_failed_set)+ len(res67_failed_set)))
 
@@ -151,12 +194,22 @@ if __name__ == "__main__":
                                                         len(res17_failed_set_graph_only),
                                                         len(res67_failed_set_graph_only), len(res42_failed_set_graph_only)+len(res17_failed_set_graph_only)+len(res67_failed_set_graph_only)))
 
+    print("\n Length of errors sets (joint): {}, {}, {} Sum: {}".format(len(res42_failed_set_joint),
+                                                        len(res17_failed_set_joint),
+                                                        len(res67_failed_set_joint), len(res42_failed_set_joint)+len(res17_failed_set_joint)+len(res67_failed_set_joint)))
+
+    print("\n Common Errors: \n", *res42_failed_set.intersection(res17_failed_set, res67_failed_set), sep="\n")
     print("\n Common Errors Graph: \n", *res42_failed_set_graph_only.intersection(res17_failed_set_graph_only, res67_failed_set_graph_only), sep="\n")
+
+    print("\n Common Errors Joint: \n", *res42_failed_set_joint.intersection(res17_failed_set_joint, res67_failed_set_joint), sep="\n")
+
 
 
     model_set_1 = res42_failed_set.intersection(res17_failed_set, res67_failed_set)
     model_set_2 = res42_failed_set_graph_only.intersection(res17_failed_set_graph_only, res67_failed_set_graph_only)
-    intersection_between_models = model_set_1.intersection(model_set_2)
+    model_set_3 = res42_failed_set_joint.intersection(res17_failed_set_joint, res67_failed_set_joint)
+
+    intersection_between_models = model_set_1 & model_set_2 & model_set_3 # model_set_1.intersection(model_set_2)
 
     print("\n Intersection of common errors: \n", *intersection_between_models, sep="\n")
 
---
+++ utils/extract_indices.py
@@ -0,0 +1,31 @@
+"""
+Retrieves the indices per verb signature
+
+"""
+
+
+def return_indices(content, count, signature):
+    index = []
+    for num, line in enumerate(content):
+        if line[14] == signature:
+            index.append(num)
+    assert len(index) == count
+    print(len(index))
+    return index
+
+
+if __name__ == "__main__":
+    f = "../data/verb_veridicality_evaluation.tsv"
+    content = []
+    with open(f, "r", encoding="utf-8") as f:
+        for line in f:
+            content.append(line.split("\t"))
+    content = content[1:]
+    plus_plus = return_indices(content, 212, signature="+/+\n")
+    plus_minus = return_indices(content, 100, signature="+/-\n")
+    minus_plus = return_indices(content, 25, signature="-/+\n")
+    neutral_plus = return_indices(content, 63, signature="o/+\n")
+    neutral_minus = return_indices(content, 28, signature="o/-\n")
+    minus_neutral = return_indices(content, 55, signature="-/o\n")
+    plus_neutral = return_indices(content, 80, signature="+/o\n")
+    neutral_neutral = return_indices(content, 935, signature="o/o\n")
