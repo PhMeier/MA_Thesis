c7e8df7d380675336571406f9168bedfabe02884
PhMeier <junk12345671@aol.com>
2022-12-19 12:00:00 +0100

    [HD] added that with create veridical from sick

--- preprocess/create_veridical_from_sick.py
+++ preprocess/create_veridical_from_sick.py
@@ -2,24 +2,6 @@
 import spacy
 import pandas as pd
 
-def read_in_verbs(filename):
-    """
-    Redas in the veridical verbs.
-    :param filename:
-    :return:
-    """
-    signature_and_verbs = {}
-    key = ""
-    with open(filename, "r", encoding="utf-8") as f:
-        for line in f:
-            line = line.split("\n")[0]
-            if "/" in line:
-                key = line
-                signature_and_verbs[key] = []
-            else:
-                signature_and_verbs[key].append(line)
-    return signature_and_verbs
-
 def pos_environment_sick(instance, verb, aux, nlp, label_verid):
     """
     Creates new veridical instances with 'to' auxiliar.
@@ -30,28 +12,51 @@ def pos_environment_sick(instance, verb, aux, nlp, label_verid):
     :param label:
     :return:
     """
+    doc = nlp(instance)  # parse the sentence
+    pos_tags = [token.pos_ for token in doc]  # get the pos tags
+    text = [token.text for token in doc]
+    lemmas = [token.lemma_ for token in doc]
     if aux == "to":
-        result = ""
-        verb = verb + "s"
-        verb_and_aux = verb + " " + aux + " "
-        doc = nlp(instance)  # parse the sentence
-        pos_tags = [token.pos_ for token in doc]  # get the pos tags
-        text = [token.text for token in doc]
-        lemmas = [token.lemma_ for token in doc]
+        pronoun, plural = find_pronoun(text[1])
+        if plural:
+            verb_and_aux = verb + " " + aux + " "
+        else:
+            verb_and_aux = verb + "s " + aux + " "
         # print(lemmas)
         if pos_tags[:4] == ['DET', 'NOUN', 'AUX', 'VERB']:
             stemmed_verb = lemmas[3]
-            hypo_verb = adapt_verb(stemmed_verb)
+            hypo_verb = adapt_verb(stemmed_verb, plural)
             result = " ".join(text[:2]) + " " + verb_and_aux + stemmed_verb + " " + " ".join(text[4:])
             hypo = " ".join(text[:2]) + " " + hypo_verb + " " + " ".join(text[4:])
             label = label_dictionary[label_verid.split("/")[0]]
             label_s2 = calculate_composite_label(label, label_verid)
-            print("{} | {} | {} | {}".format(result, hypo, label, label_s2))
+            print("{} | {} | {}".format(result, hypo, label))
             return result, hypo, label, label_s2
         else:
             return "", "", "", ""
     else:
-        return "", "", "", ""
+        # "The woman is dicing garlic" The woman does not forget that she is dicing garlic
+        pronoun, plural = find_pronoun(text[1])
+        if plural:
+            verb_and_aux = verb + " " + aux + " "
+        else:
+            verb_and_aux = verb + "s " + aux + " "
+        if pos_tags[:4] == ['DET', 'NOUN', 'AUX', 'VERB']:
+            complement_aux_and_verb = text[2] + " " + text[3] + " "
+            stemmed_verb = lemmas[3]
+            hypo_verb = adapt_verb(stemmed_verb, plural)
+            result = " ".join(text[:2]) + " " + verb_and_aux + pronoun + " " + complement_aux_and_verb + " ".join(
+                text[4:])
+            hypo = " ".join(text[:2]) + " " + hypo_verb + " " + " ".join(text[4:])
+            label = label_dictionary[label_verid.split("/")[1]]
+            label_s2 = calculate_composite_label(label, label_verid)
+            print("{} | {} | {}".format(result, hypo, label))
+            return result, hypo, label, label_s2
+            # print(result)
+            # print(hypo)
+        else:
+            return "", "", "", ""
+
 
 
 def neg_environment_sick(instance, verb, aux, nlp, label_verid):
@@ -64,26 +69,56 @@ def neg_environment_sick(instance, verb, aux, nlp, label_verid):
     :param label:
     :return:
     """
+    doc = nlp(instance)  # parse the sentence
+    pos_tags = [token.pos_ for token in doc]  # get the pos tags
+    text = [token.text for token in doc]
+    lemmas = [token.lemma_ for token in doc]
     if aux == "to":
         verb_and_aux = "does not " + verb + " " + aux + " "
-        doc = nlp(instance)  # parse the sentence
-        pos_tags = [token.pos_ for token in doc]  # get the pos tags
-        text = [token.text for token in doc]
-        lemmas = [token.lemma_ for token in doc]
         if pos_tags[:4] == ['DET', 'NOUN', 'AUX', 'VERB']:  # first four words of the sick instance need this tags
             stemmed_verb = lemmas[3]
-            hypo_verb = adapt_verb(stemmed_verb)
+            pronoun, plural = find_pronoun(text[1])
+            hypo_verb = adapt_verb(stemmed_verb, plural)
             result = " ".join(text[:2]) + " " + verb_and_aux + stemmed_verb + " " + " ".join(text[4:])
             hypo = " ".join(text[:2]) + " " + hypo_verb + " " + " ".join(text[4:])
             label = label_dictionary[label_verid.split("/")[1]]
             label_s2 = calculate_composite_label(label, label_verid)
-            print("{} | {} | {} | {}".format(result, hypo, label, label_s2))
+            print("{} | {} | {}".format(result, hypo, label))
             return result, hypo, label, label_s2
         else:
             return "", "", "", ""
     else:
-        return "", "", "", ""
+        # "The woman is dicing garlic" The woman does not forget that she is dicing garlic
+        verb_and_aux = "does not " + verb + " " + aux + " "
+        if pos_tags[:4] == ['DET', 'NOUN', 'AUX', 'VERB']:
+            complement_aux_and_verb = text[2] + " " + text[3] + " "
+            pronoun, plural = find_pronoun(text[1])
+            stemmed_verb = lemmas[3]
+            hypo_verb = adapt_verb(stemmed_verb, plural)
+            result = " ".join(text[:2]) + " " + verb_and_aux + pronoun + " " + complement_aux_and_verb + " ".join(text[4:])
+            hypo = " ".join(text[:2]) + " " + hypo_verb + " " + " ".join(text[4:])
+            label = label_dictionary[label_verid.split("/")[1]]
+            label_s2 = calculate_composite_label(label, label_verid)
+            print("{} | {} | {}".format(result, hypo, label))
+            return result, hypo, label, label_s2
+            #print(result)
+            #print(hypo)
+        else:
+            return "", "", "", ""
+    return "", "", "", ""
 
+def find_pronoun(text):
+    it = ["monkey", "dog","cat", "puppy", "person"]
+    female = ["girl", "woman", "girl", "lady"]
+    plural = ["animals", "men", "ferrets", "people", "dogs"]
+    if text in female:
+        return "she", False
+    if text in it:
+        return "it", False
+    if text in plural:
+        return "they", True
+    else:
+        return "he", False
 
 def calculate_composite_label(label_1, label_verid):
     """
@@ -91,7 +126,7 @@ def calculate_composite_label(label_1, label_verid):
     :param label:
     :return:
     """
-    label_list = ["Minus/Neutral", "Neutral/Minus"]  # these labels cause an unknwon for the composite
+    label_list = ["Minus/Neutral", "Neutral/Minus", "Neutral/Plus"]  # these labels cause an unknwon for the composite
     # for these signatures the label f(s1) --> s2 is equal to f(s1) --> s1
     set_equal = ["Minus/Plus", "Plus/Minus", "Plus/Neutral", "Plus/Plus", "Neutral/Neutral"]
     if label_verid in set_equal:
@@ -116,12 +151,14 @@ def active_passive_checker(instance, nlp):
         return False
 
 
-def adapt_verb(verb):
+def adapt_verb(verb, plural):
     """
     Adapt the given verb to the third person according to its ending.
     :param verb:
     :return verb:
     """
+    if plural:
+        return verb
     if verb.endswith("y"):
         if verb.endswith("ay"):
             verb = verb + "s"
